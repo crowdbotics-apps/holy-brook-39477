a76174c1182476f3e204837637896bc6544bd9ac
Crowdbotics <team@crowdbotics.com>
2023-03-21 12:17:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-brook-39477",
+  url: "https://holy-brook-39477.botics.co",
+  api: "https://holy-brook-39477.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
